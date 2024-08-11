317701029a02ea04b05429e98cb9b576b259dee8
whitetern <dvlpjh@gmail.com>
2024-08-11 17:44:51 +0900

    first commit

---
+++ src/main/java/com/x86s/loginexamples/LoginExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.x86s.loginexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/x86s/loginexamples/LoginExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x86s.loginexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
